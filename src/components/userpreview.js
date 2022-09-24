de61e222454c98df9bbf7cffd37fb942bbf69dfe
Sumit Kumar <80626529+SumitKumar-2000@users.noreply.github.com>
2022-09-25 03:47:48 +0530

    [userPreview]

--- src/components/userpreview.js
+++ src/components/userpreview.js
@@ -5,6 +5,7 @@ import Spline from '@splinetool/react-spline';
 const UserPreview = () => {
   return (
     <div className='userPreviewContainer' id='home'>
+      {/* <Spline scene="https://prod.spline.design/6chrG-SLuZMb9JqB/scene.splinecode" /> */}
       <Spline scene="https://prod.spline.design/6chrG-SLuZMb9JqB/scene.splinecode" />
     </div>
   )
