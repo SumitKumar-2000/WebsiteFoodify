84c24570ad8eadf3200fb6c45ef4c2d9cb2aeafa
SumitKumar-2000 <sumitkumarracer@gmail.com>
2022-10-27 20:03:31 +0530

    Re-Deployment done

---
+++ src/components/test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+        <div>
+            <img src={require('../assets/cummunity.jpeg')}/>
+        </div>
+        <div>
+            <img src={require('../assets/solve.jpeg')}/>
+        </div>
+        <div>
+            <img src={require('../assets/survey.jpeg')}/>
+        </div>
+        <div>
+            <img src={require('../assets/why.jpeg')}/>
+        </div>
+    </div>
+  )
+}
+
+export default Test
